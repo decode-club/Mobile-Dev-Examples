f8ffaccebddc8676c6bd074a75d3d70f513e6392
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-18 15:49:59 +0100

    example-02

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("This is a print message in the log")
+        Log.i("Logging App", "this is an information message")
+        Log.v("Logging App", "this is a verbose message")
+        Log.d("Logging App", "this is a debug message")
+        Log.w("Logging App", "this is a warning message")
+        Log.e("Logging App", "this is an error message")
+        Log.wtf("Logging App", "this is a WTF message")
     }
 }
