c468b07fe78f9a14257db253658c35fcd01f2b27
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-25 17:52:50 +0100

    example 10

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _textview = findViewById<TextView>(R.id.textview)
+        _button = findViewById<Button>(R.id.button)
+        _button?.setOnClickListener ( object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                _clicks++
+                _textview?.setText("This button has been clicked " +_clicks + " times")
+            }
+        })
     }
+    // private fields of the class
+    private var _textview: TextView? = null
+    private var _button: Button? = null
+    private var _clicks: Int = 0
 }
