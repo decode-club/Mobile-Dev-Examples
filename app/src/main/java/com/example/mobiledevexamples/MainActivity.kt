bdffc2fb82f4a26feae6aed0860c331eb0a72d02
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-25 17:49:07 +0100

    example 09

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get a reference to the textview and change its contents
+        var tv_textview = findViewById<TextView>(R.id.textview)
+        tv_textview.setText("changed from the Kotlin Code")
     }
 }
