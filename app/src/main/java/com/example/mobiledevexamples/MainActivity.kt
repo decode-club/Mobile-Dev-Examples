33ce1e9b1b998d37b4184ba14ce97697832f75da
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-27 09:25:30 +0100

    example 26

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // log a message to indicate that this lifecycle handler has been called
+        Log.i("MainActivity", "onCreate() called")
+    }
+    // 03 code goes here
+    // overridden onDestroy function
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy() called")
+    }
+    // 04 code goes here
+    // overridden resume function
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume() called")
+    }
+    // 05 code goes here
+    // overridden pause function
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause() called")
+    }
+    // 06 code goes here
+    // overridden stop function
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop() called")
+    }
+    // 07 code goes here
+    // overridden start function
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart() called")
     }
 }
