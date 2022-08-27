2cc927d8a8afe194f245588e6c06be8a199ee70c
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-27 07:41:30 +0100

    example 16

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.CalendarView
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _textview = findViewById<TextView>(R.id.textview)
+        _calendarview = findViewById<CalendarView>(R.id.calendar)
+        _calendarview?.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
+            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+                _textview?.setText("selected date (YYYY-MM-DD): " + p1 + "-" + p2 + "-" + p3)
+            }
+        })
     }
+    private var _textview: TextView? = null
+    private var _calendarview: CalendarView? = null
 }
