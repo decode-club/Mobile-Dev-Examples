7a184d1a3abda8f54949948d0cc72dcfc24db24b
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-08-27 07:39:37 +0100

    example 15

--- app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevexamples/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mobiledevexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.NumberPicker
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _textview = findViewById<TextView>(R.id.textview)
+        _numberpicker = findViewById<NumberPicker>(R.id.numberpicker)
+        _numberpicker?.setMinValue(0)
+        _numberpicker?.setMaxValue(10)
+        _numberpicker?.setOnValueChangedListener(object :
+            NumberPicker.OnValueChangeListener {
+            override fun onValueChange(p0: NumberPicker?, p1: Int, p2: Int) {
+                _textview?.setText("user has selected: " + p2)
+            }
+        })
     }
+    var _textview: TextView? = null
+    var _numberpicker: NumberPicker? = null
 }
